714ec4d93617590d48b4389af93366cf1c4c3e28
nishalini2 <114395166+nishalini2@users.noreply.github.com>
2023-08-25 04:47:56 -0700

    Delete erp directory

--- erp/src/main/java/com/isys/erp/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isys.erp.repository;
-
-import com.isys.erp.entity.CompanyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
-}
--- erp/src/main/java/com/isys/erp/service/Service/CompanyService.java
+++
@@ -1,9 +0,0 @@
-package com.isys.erp.service.Service;
-
-import com.isys.erp.dto.CompanyDto;
-import org.springframework.http.ResponseEntity;
-
-public interface CompanyService {
-    ResponseEntity<CompanyDto>createCompany (CompanyDto companyDto);
-    CompanyDto GetByUserId (Long id);
-}
