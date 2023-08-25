f1017e4e37e1f3f7f3e9fbdc0cd16bb557cf3f08
nishalini2 <114395166+nishalini2@users.noreply.github.com>
2023-08-25 04:47:36 -0700

    Delete comdata directory

--- comdata/src/main/java/com/isys/erp/comdata/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.isys.erp.comdata.repository;
-
-import com.isys.erp.comdata.entity.CompanyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<CompanyEntity,Long> {
-}
--- comdata/src/main/java/com/isys/erp/comdata/service/Service/CompanyService.java
+++
@@ -1,7 +0,0 @@
-package com.isys.erp.comdata.service.Service;
-
-import com.isys.erp.comdata.dto.CompanyDto;
-
-public interface CompanyService {
-    CompanyDto createCompany(CompanyDto companyDto);
-}
